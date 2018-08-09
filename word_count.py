83dc6c9b83ce09b3fbf1d0e5feb0ebb824a709f0
Süleyman Yaman <suleymanyamantr@gmail.com>
2018-08-09 15:11:11 +0300

    added

---
+++ word_count.py
@@ -0,0 +1,24 @@
+import docx2txt
+import re
+
+my_text = docx2txt.process("The directory to your .docx file here").lower()
+
+
+match = re.findall(r'[a-zA-Z\’\d\'\-]+',my_text)
+
+
+
+data={}
+
+for word in match:
+    if word not in data:
+        data[word]=1
+    else:
+        data[word]+=1
+
+
+print("Unique words:",len(data.keys()))
+print("Words:",  len(match))
+
+
+c = input("")
